ff5d613e9402daf0d8e9182d092d5c2df2384ddb
Jacob Friis Saxberg <jacob@webcom.dk>
2021-08-04 18:25:00 +0200

    test WIP

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,10 +4,13 @@ import { readFileSync, writeFileSync } from 'fs';
 import gpx2geo from '../src/index';
 
 const file = './assets/test.gpx';
+// const file = './assets/activity_7239122045.gpx';
+// const file = './assets/activity_7240644361.gpx';
+// const file = './assets/Waterspeed 2021-08-03T12.17.40.000Z.gpx';
 
 describe('gpx2geo', () => {
 
-  it('should be able to parse a GPX file', function() {
+  xit('should be able to parse a GPX file', function() {
     // this.timeout(10000);
     const buffer:ArrayBuffer = readFileSync(file);
     writeFileSync('./assets/geo.json', JSON.stringify(gpx2geo(buffer)));
@@ -15,8 +18,7 @@ describe('gpx2geo', () => {
 
   it('should be able to parse a GPX file', function() {
     // this.timeout(10000);
-    const buffer:ArrayBuffer = readFileSync(file);
-    console.log(gpx2geo(buffer));
+    console.log(gpx2geo(readFileSync(file)));
   });
 
 });
